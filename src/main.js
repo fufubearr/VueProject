804e035cf6383c76f177e3b3009865dff5a389c1
fufubear <fufu258w@gmail.com>
2018-12-25 17:11:29 +0800

    ＋normalize.css

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import 'bootstrap'
 // import 'bootstrap/dist/css/bootstrap.css'
-import App from './App'
-import router from './router'
+
+// Ajax
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
